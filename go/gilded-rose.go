4fb58e8e58162d5da0439a8ea06930046959b4a4
Tom Toledo <tomt@recolabs.ai>
2022-03-23 14:10:31 +0200

    refactoring gilded-rose. SOLID

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -1,57 +1,124 @@
 package main
 
+import "fmt"
+
 type Item struct {
 	name            string
 	sellIn, quality int
 }
 
+func (item *Item) Name() string {
+	return item.name
+}
+
+func (item *Item) addQuality(val int) bool {
+	item.quality = item.quality + val
+	return true
+}
+
+func (item *Item) incQuality() bool {
+	item.addQuality(1)
+	return true
+}
+
+func (item *Item) decQuality() bool {
+	item.addQuality(-1)
+	return true
+}
+
+func (item *Item) resetQuality() bool {
+	item.quality = 0
+	return true
+}
+
+func (item *Item) Quality() int {
+	return item.quality
+}
+
+func (item *Item) addSellIn(val int) bool {
+	item.sellIn = item.sellIn + val
+	return true
+}
+
+func (item *Item) decSellIn() bool {
+	item.addSellIn(-1)
+	return true
+}
+
+func (item *Item) SellIn() int {
+	return item.sellIn
+}
+
+func (item *Item) defaultCase() error {
+	if item.Quality() > 0 {
+		item.decQuality()
+		if item.SellIn() <= 0 && item.Quality() > 0 {
+			item.decQuality()
+		}
+	}
+	item.decSellIn()
+
+	return nil
+}
+
+func (item *Item) sulfurasCase() error {
+	return nil
+}
+
+func (item *Item) backstageCase() error {
+	item.decSellIn()
+	if item.SellIn() < 0 {
+		item.resetQuality()
+		return nil
+	}
+
+	if item.Quality() < 50 {
+		item.incQuality()
+		if item.SellIn() < 10 && item.Quality() < 50 {
+			item.incQuality()
+		}
+		if item.SellIn() < 5 && item.Quality() < 50 {
+			item.incQuality()
+		}
+	}
+
+	return nil
+}
+
+func (item *Item) agedBrieCase() error {
+	if item.Quality() < 50 {
+		item.incQuality()
+		if item.SellIn() <= 0 && item.Quality() < 50 {
+			item.incQuality()
+		}
+	}
+	item.decSellIn()
+	return nil
+}
+
+func (item *Item) update() error {
+
+	switch item.Name() {
+
+	case "Aged Brie":
+		return item.agedBrieCase()
+
+	case "Backstage passes to a TAFKAL80ETC concert":
+		return item.backstageCase()
+
+	case "Sulfuras, Hand of Ragnaros":
+		return item.sulfurasCase()
+
+	default:
+		return item.defaultCase()
+
+	}
+}
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
-
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
-		}
-
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
-			}
+		if err := items[i].update(); err != nil {
+			fmt.Println("item", items[i], ",update Error", err.Error())
 		}
 	}
 
